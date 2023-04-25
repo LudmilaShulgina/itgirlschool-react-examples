8173f5c984dfd4bdb94e55fd6b012e90a50afde5
LudmilaShulgina <ludmila.shulgina@global-fashion-group.com>
2023-04-25 16:10:15 +0300

    Functional component with props

--- src/App.js
+++ src/App.js
@@ -4,7 +4,24 @@ import Card from "./components/Card";
 function App() {
   return (
     <div className="App">
-      <Card />
+      <Card
+        imageUrl="https://i.pinimg.com/originals/d0/32/89/d032898ae807199292bd01b7e4b43655.jpg"
+        title="Базовые сапожки"
+        description="Подойдут под любой наряд"
+        price="$23.99"
+      />
+      <Card
+        imageUrl="https://img.vitkac.com/uploads/product_thumb/BUTY%20538942%20W04Z0-3801/up/1.JPG"
+        title="Зеленые сапожки"
+        description="Когда на душе весна"
+        price="$23.99"
+      />
+      <Card
+        imageUrl="https://m.media-amazon.com/images/I/61G56PZgCOL._AC_SX575._SX._UX._SY._UY_.jpg"
+        title="Шлепки нарядные"
+        description="Для особыч случаев"
+        price="$99.99"
+      />
     </div>
   );
 }
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,16 +1,12 @@
 import "./card.css";
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
-      <img
-        src="https://i.pinimg.com/originals/d0/32/89/d032898ae807199292bd01b7e4b43655.jpg"
-        alt="Базовые сапожки"
-        className="card-image"
-      />
-      <h2 className="card-title">Базовые сапожки</h2>
-      <p className="card-description">Эклектичное сочетание ...</p>
-      <p className="card-price">$23.90</p>
+      <img src={props.imageUrl} alt={props.title} className="card-image" />
+      <h2 className="card-title">{props.title}</h2>
+      <p className="card-description">{props.description}</p>
+      <p className="card-price">{props.price}</p>
       <button className="card-button">Заказать</button>
     </div>
   );
